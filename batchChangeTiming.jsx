5656e9caeae8130c198135d66dc8a6c138ef7771
Nik Ska <dokluch@gmail.com>
2013-07-10 13:22:32 +0400

    nik's snippets

---
+++ batchChangeTiming.jsx
@@ -0,0 +1,60 @@
+﻿//script used for batch change timing of selected compositions
+//or actve comp with all layers
+//useful when working with 3d passes
+//Nik Ska, 2013
+
+
+var chTiming = this;
+
+chTiming.run = function(){
+    this.buildGUI(this);
+    }
+
+chTiming.buildGUI = function(thisObj){
+	thisObj.w = (thisObj instanceof Panel) ? thisObj : new Window("palette", thisObj.scriptTitle, undefined, {resizeable:true});
+	thisObj.w.alignChildren = ['left', 'top']
+	thisObj.w.add("staticText", undefined, "Batch Timing Changer 0.1");
+	var g = thisObj.w.add("group{orientation:'row', alignChildren: ['left', 'top']}");
+	var timeText = g.add("editText", undefined, "0");
+	timeText.size = [40, 20];
+	var modeselect = g.add("dropdownlist", undefined, ["s", "fr"]);
+	modeselect.selection = 0;
+
+	var g2 = thisObj.w.add("group{orientation:'row', alignChildren: ['left', 'top']}");
+	var recChck = g2.add("checkbox", undefined, "Recursive");
+
+	timeText.onEnterKey = function(){
+		thisObj.changeTiming(Number(timeText.text), modeselect.selection.index, recChck.value);
+	}
+
+	if (thisObj.w instanceof Window){
+    thisObj.w.center();
+    thisObj.w.show();
+  }
+  else thisObj.w.layout.layout(true);
+}
+
+chTiming.changeTiming = function(_time, _sel, recursive){
+    function loopthrough(){
+      for(var i = 0; i<vids.length; i++){
+				if(_sel == 1){_time*=vids[i].frameDuration} //frames
+				vids[i].duration = _time;
+				for(var k = 1; k<=vids[i].layers.length; k++){
+					vids[i].layers[k].outPoint = vids[i].duration;
+				}
+			}
+    }
+	var sel_vids = app.project.selection;
+		if(sel_vids.length>0){
+			var vids = sel_vids;
+      loopthrough()
+		}
+		else if(app.project.activeItem){
+			var vids = [app.project.activeItem];
+      loopthrough()
+		}
+
+		
+}
+
+chTiming.run()
---
+++ debughelper.jsx
@@ -0,0 +1,34 @@
+﻿//debug helper - creates a small panel with text layer linked to all selected properties
+//now works in a comand line mode
+
+var activeItem = app.project.activeItem;
+
+var activeItem = app.project.activeItem;
+if (activeItem != null && activeItem instanceof CompItem){
+	var sel = activeItem.selectedProperties;
+	if(sel){
+		var prevLayerIndex = -1;
+		for(var i = 0 ; i < sel.length ; i++){
+			if(sel[i].propertyValueType){
+				if(getParentLayer(sel[i]).index != prevLayerIndex){
+					$.writeln("\n====================");
+					$.writeln(getParentLayer(sel[i]).name);
+					$.writeln("====================");
+					prevLayerIndex = getParentLayer(sel[i]).index;
+				}
+				$.writeln(sel[i].name + ': ' + String(sel[i].value));
+			}
+		}
+	}
+}
+
+function getParentLayer(_property){
+    //recursively gets the parent layer
+    if(_property.propertyDepth>0){
+        return getParentLayer(_property.parentProperty);
+        }
+    else{
+        return _property;
+        }
+    }
+        
