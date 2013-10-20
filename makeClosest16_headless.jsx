7c535baa62aa630b823221667307296da787b3ad
Nik Ska <dokluch@gmail.com>
2013-10-20 15:29:53 +0400

    upd

--- makeClosest16_headless.jsx
+++ makeClosest16_headless.jsx
@@ -8,15 +8,18 @@ var activeComp = app.project.activeItem;
 
 if(activeComp != null && activeComp instanceof CompItem){
 	var compSize = [activeComp.width, activeComp.height];
+	
 	app.beginUndoGroup("Resize to closest 16");
-    activeComp.width = getClosest16(compSize[0]);
-    activeComp.height = getClosest16(compSize[1]);
+	
+	activeComp.width = getClosest16(compSize[0]);
+	activeComp.height = getClosest16(compSize[1]);
 
-    app.endUndoGroup();
+	app.endUndoGroup();
 
     }
 
 var getClosest16 = function(a){
 	if(a%16 == 0) return a;
+	else if (a%16 < 4) return 16*Math.floor(a/16);
 	else return 16*(Math.floor(a/16)+1);
 }
