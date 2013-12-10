b9d02e4cb6683dfade905292b9a07b86d5e4114d
Nik Ska <dokluch@gmail.com>
2013-12-10 13:30:28 +0400

    new scripts for precomps

--- nicePrecomp.jsx
+++ nicePrecomp.jsx
@@ -5,15 +5,12 @@
 var precompFixer = {}
 
 precompFixer.go = function(){
-
 	function getTimes(_sel){
-		
-		var end = 0;
+		var end = 0; //ищем конец последнего слоя
 		for(var e = 1; e <= _sel.length; e++){
 			if(_sel[e].outPoint>end) end = _sel[e].outPoint;
 		}
-
-		var begin = end;
+		var begin = end; //ищем начало первого слоя
 		for(var b = 1; b <= _sel.length; b++){
 			if(_sel[b].inPoint<begin) begin = _sel[b].inPoint;
 		}
@@ -25,16 +22,14 @@ precompFixer.go = function(){
 		var sel = activeComp.selectedLayers; //выбранные слои
 		app.beginUndoGroup("Nice Precomp"); //начинаем Undo
 		if(sel.length == 1 && sel[0].source instanceof CompItem){ //если мы выбрали прекомп
-			var precomp = sel[0].source;
-			var timings = getTimes(precomp.layers);
-			precomp.duration = timings[1]-timings[0];
-			precomp.displayStartTime = timings[0];
-			sel[0].startTime+=timings[0];
+			var precomp = sel[0].source; //берем сам прекомп
+			var timings = getTimes(precomp.layers); //рассчитываем начало и конец
+			precomp.duration = timings[1]-timings[0]; //рассчитываем новую длину
+			precomp.displayStartTime = timings[0]; //сдвигаем Display Start Time
+			sel[0].startTime+=timings[0]; //сдвигаем сам прекомп
 			for(var l = 1 ; l <= precomp.layers.length ; l++){
-				precomp.layers[l].startTime -= timings[0]
+				precomp.layers[l].startTime -= timings[0]; //сдвигаем слои внутри
 			}
-			//precomp
-			//precomp.startTime = timings[0]
 		}
 		app.endUndoGroup(); //закрываем Undo
 	}
