e943ee092f5aa4b0a6d8b431db80cb28e491f6a0
Nik Ska <dokluch@gmail.com>
2015-01-08 10:27:30 +0300

    no message

--- README.md
+++ README.md
@@ -4,3 +4,13 @@ This repo has been built over the years as the result of working on [vk.com comm
 
 Some of these scripts are unfinished or may work not as expected. 
 But that's the point of education.
+
+### Contents
+
+*addRoundedShape.jsx – Just adds a blank shape object with rounded Line Cap and Line Join
+*batchChangeTiming.jsx – Script used for batch change timing of selected compositions or actve comp with all layers
+*batchParent.jsx – parent a bunch of layers to another bunch
+*changeAllFonts.jsx – changes all fonts in a composition to a given one
+*copyMarkersToComp.jsx – *unfinished* an experiment to transfer markers from a layer to compotion
+*csvreader.jsx – an example of parsing csv file into AE comps
+*debughelper.jsx – *unfinished* an experiment to create a text panel connected to selected properties
--- csvreader.jsx
+++ csvreader.jsx
@@ -1,16 +1,16 @@
-﻿function findCompByName(_name){
-	for(var i = 1; i <= app.project.numItems; i++){
-		if(app.project.item(i).name == _name && app.project.item(i) instanceof CompItem){
-			return app.project.item(i);
-		}
+﻿//This script is an example of parsing csv file into AE comps
+
+function findCompByName(_name){
+	for(var i=1;i<=app.project.numItems;i++){
+		if(app.project.item(i).name==_name && app.project.item(i) instanceof CompItem) return app.project.item(i);
 	}
-	return null;
+	return null
 }
 
 function findEffectByName(_name, _layer){
 	//looks for an effect with given name inside given layer
 	var eff = _layer.property("ADBE Effect Parade");
-	for(var e = 1; e <= eff.numProperties; e++){
+	for(var e = 1; e<= eff.numProperties; e++){
 		if(eff.property(e).name == _name) return eff.property(e);
 	}
 	return null;
@@ -19,9 +19,8 @@ function findEffectByName(_name, _layer){
 function getFolder(_name){
 	//function for gettin a specific folder
 	var bFolder;
-
 	for(var i=1;i<=app.project.numItems;i++){
-		if(app.project.item(i).name == _name && app.project.item(i) instanceof FolderItem) bFolder = app.project.item(i);
+		if(app.project.item(i).name==_name && app.project.item(i) instanceof FolderItem) bFolder = app.project.item(i);
 	}
 
 	//if ther is no such folder, add it
@@ -29,44 +28,34 @@ function getFolder(_name){
 	return bFolder
 }
 
-
-//open file
 var textFile = File.openDialog ("Choose a csv file","*.csv");
 
 if (textFile != null) {
-	//initialize array
     var textLines = [];
     textFile.open("r"); 
-
-
-	while(!textFile.eof){
-	    //reading file into lines
-	    textLines.push(textFile.readln());
-	}
-
-	app.beginUndoGroup("Reading csv");
-
-	//making or getting folder
-	targetFolder = getFolder("broadcast");
-	var template = findCompByName("info_template");
-
-	for(var i = 1; i<textLines.length; i++){
-
-		var line = textLines[i].split(",");
-		
-		var newComp = template.duplicate();
-		newComp.name = "infoline_" + String(i);
-		newComp.parentFolder = targetFolder;
-
-		//accessing variables
-		var nameText = newComp.layer("name").property("ADBE Text Properties").property("ADBE Text Document");
-		var regionText = newComp.layer("region").property("ADBE Text Properties").property("ADBE Text Document");
-		var numberValue = findEffectByName("amount", newComp.layer("number")).property("ADBE Slider Control-0001");
-
-		//setting variables
-		nameText.setValue(line[0]);
-		regionText.setValue(line[1]);
-		numberValue.setValueAtKey(2, Number(line[2])/100000);
-	}
-	app.endUndoGroup();
 }
+
+while (!textFile.eof){
+    //reading file into lines
+    textLines.push(textFile.readln()); 
+}
+
+var template = findCompByName("info_template"); //reference comp
+var bFolder = getFolder("broadcast");
+app.beginUndoGroup("tst");
+for(var t = 1; t<textLines.length; t++){
+	var newComp = template.duplicate();
+	newComp.name = "info_"+String(t);
+	newComp.parentFolder = bFolder;
+
+	var infoName = newComp.layer("name").property("ADBE Text Properties").property("ADBE Text Document");
+	var infoRegion = newComp.layer("region").property("ADBE Text Properties").property("ADBE Text Document");
+	var infoNumber = findEffectByName("amount",  newComp.layer("number").property("ADBE Slider Control-0001");
+
+	var infoLine = textLines[t].split(',');
+	infoName.setValue(infoLine[0]);
+	infoRegion.setValue(infoLine[1]);
+	infoNumber.setValueAtTime(infoNumber.keyTime(2), Number(infoLine[2])/100000);
+}
+
+app.endUndoGroup();
