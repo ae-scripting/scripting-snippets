04183c6e37cfa4796c5637f4ffd4ce83508793b7
Nik Ska <dokluch@gmail.com>
2015-01-19 15:57:35 +0300

    Updated dynamic box

--- dynamic_bbox.jsx
+++ dynamic_bbox.jsx
@@ -10,13 +10,13 @@ CompItem.prototype.dd_dynamicBbox_createBbox = function(_layer)
 
 	//shape match names:
 	var contentsName = "ADBE Root Vectors Group";
-	var vectorGroupName = "ADBE Vector Group";//любая группа с шейпом
-		var rectPathName = "ADBE Vector Shape - Rect";//группа с ректанглом
-			var rectPathSizeName = "ADBE Vector Rect Size";//size ректангла
-	var strokeGroupName = "ADBE Vector Graphic - Stroke";//весь stroke
+	var vectorGroupName = "ADBE Vector Group";//a shape group
+		var rectPathName = "ADBE Vector Shape - Rect";//rectangle group
+			var rectPathSizeName = "ADBE Vector Rect Size";//rectangle size
+	var strokeGroupName = "ADBE Vector Graphic - Stroke";//stroke
 		var strokeWidthName = "ADBE Vector Stroke Width";//stroke width
 		var strokeColorName = "ADBE Vector Stroke Color";//stroke color
-	var fillGroupName = "ADBE Vector Graphic - Fill";//весь fill
+	var fillGroupName = "ADBE Vector Graphic - Fill";//fill
 		var fillColorName = "ADBE Vector Fill Color";////fill color
 
 	//effect match names:
@@ -32,22 +32,22 @@ CompItem.prototype.dd_dynamicBbox_createBbox = function(_layer)
 	var shapePosExpression = "textLayer = effect(\"target layer\")(\"Layer\");\nvar strokes = effect(\"accept strokes\")(\"Checkbox\");\nbbox = textLayer.sourceRectAtTime(time,strokes);\ntextLayer.position-textLayer.anchorPoint+\n	[bbox.left*textLayer.scale[0]/100,bbox.top*textLayer.scale[1]/100]+\n	[bbox.width*textLayer.scale[0]/100,bbox.height*textLayer.scale[1]/100]/2;";
 	var textPosExpression = "textLayer = effect(\"target layer\")(\"Layer\");\nbbox = textLayer.sourceRectAtTime(time,false);\ntextLayer.position-textLayer.anchorPoint+\n	[bbox.left*textLayer.scale[0]/100,bbox.top*textLayer.scale[1]/100]+\n	[bbox.width*textLayer.scale[0]/100,bbox.height*textLayer.scale[1]/100]/2;";
 
-	//создаем шейп
+	//creating shape layer
 	var newShape = this.layers.addShape();
 	newShape.name = _layer.name+"_bbox";
 	newShape.moveAfter(_layer);
 	
-	//создаем все контролы
+	//creating all controls
 	var layerControl = newShape.property(effectsName).addProperty(layerControlName);
-		layerControl.name = "target layer";
+		layerControl.name = "Target Layer";
 		layerControl.property(layerControlName+"-0001").setValue(_layer.index);
 	var sliderControl = newShape.property(effectsName).addProperty(sliderControlName);
-		sliderControl.name = "add space";
+		sliderControl.name = "Padding";
 		sliderControl.property(sliderControlName+"-0001").setValue(bboxProps.addSpace);
 	if(_layer instanceof ShapeLayer)
 		{
 			var checkboxControl = newShape.property(effectsName).addProperty(cbControlName);
-			checkboxControl.name = "accept strokes";
+			checkboxControl.name = "Accept Strokes";
 			checkboxControl.property(cbControlName+"-0001").setValue(true);
 		}
 
@@ -74,23 +74,23 @@ function dd_dynamicBbox()
 	vers = parseFloat(app.version);
 	if(vers<13.2)
 		{
-			alert("Old After Effects version: "+vers+". It should be at least 13.2");
+			alert("Update to After Effects CC2014 or later to use this script");
 			return 0;
 		}
 	var myComp = app.project.activeItem;
 	if(!myComp || !(myComp instanceof CompItem) || myComp.selectedLayers.length==0)
 		{
-			alert("Choose layers!");
+			alert("Choose at least one layer");
 			return 0;
 		}
 	
-	var arr = []//массив слоёв, с которыми можно работать: шейпы и тексты
+	var arr = []//layers array (only text and/or shape layers)
 	for(var i=0;i<myComp.selectedLayers.length;i++)
 		if(myComp.selectedLayers[i] instanceof TextLayer || myComp.selectedLayers[i] instanceof ShapeLayer)
 			arr.push(myComp.selectedLayers[i]);
 	if(arr.length == 0)
 		{
-			alert("Select Shape or Text layers!");
+			alert("Select Shape or Text layer");
 			return 0;
 		}
 	for(var i=0;i<arr.length;i++)
