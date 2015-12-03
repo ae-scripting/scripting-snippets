2442c42cac925af81c7f5ecc5518ccec5bb11c7f
Nik Ska <dokluch@gmail.com>
2015-12-03 13:15:47 +0300

    Fixed "unlock" error for batchChangeTiming.jsx Updated filterInput.jsx (new regex)

--- filterInput.jsx
+++ filterInput.jsx
@@ -1,6 +1,6 @@
 //snippet to filter edittext input to only numbers
 
 edittext.onChanging = function(){
-	preFilter = /[+-]?([0-9\:\.]*)/; //this one does not allow to input anything but numbers and some symbols;
+	preFilter = /([0-9]*)/; //this one does not allow to input anything but numbers and some symbols;
 	this.text = this.text.match(preFilter)[0];
 }
