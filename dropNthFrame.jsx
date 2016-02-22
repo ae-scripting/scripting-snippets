d9506df4b5241ae77e5d9eba4a943d966b4a64d2
Nik Ska <dokluch@gmail.com>
2016-02-23 00:20:31 +0300

    Drop Nth frame now works

--- dropNthFrame.jsx
+++ dropNthFrame.jsx
@@ -1,24 +1,1 @@
-//Script drops every Nth frame starting from a given frame
-
-var activeItem = app.project.activeItem;
-if(activeItem != null && activeItem instanceof CompItem){
-    
-    var frameStart = 3; //when to start
-    var delta = 6; //step
-    var fd = activeItem.frameDuration;
-    var sel = activeItem.selectedLayers;
-    if(sel != null){
-        if(sel[0].timeRemapEnabled == false) sel[0].timeRemapEnabled = true;
-        else{
-            var curFrame = frameStart*fd;
-            var tr = sel[0].property("ADBE Time Remapping");
-            var shift = 0;
-            while(curFrame<sel[0].source.duration){
-                tr.setValueAtKey(tr.addKey(curFrame-shift), curFrame); //add first
-                tr.setValueAtKey(tr.addKey((curFrame+fd-shift)), (curFrame+2*fd)); //add second +shift
-                curFrame+=delta*fd; //increment frame
-                shift+=fd; //increment shift
-            }
-        }
-    }
-}   
+﻿//Script drops every Nth frame starting from a given framevar activeItem = app.project.activeItem;if(activeItem != null && activeItem instanceof CompItem){        var frameStart = 0; //when to start    var delta = 5; //Drop this frame    var fd = activeItem.frameDuration;    var sel = activeItem.selectedLayers;    if(sel != null){        if(sel[0].timeRemapEnabled == false){             sel[0].timeRemapEnabled = true;        }        delta--;        var curFrame = frameStart*fd;        var tr = sel[0].property("ADBE Time Remapping");        var shift = 0;        app.beginUndoGroup("Dropping every " + String(delta+1) + " frame");        while((curFrame+fd*(delta))<sel[0].source.duration){            tr.setValueAtKey(tr.addKey(curFrame-shift*fd), curFrame); //add first            tr.setValueAtKey(tr.addKey(curFrame+fd*(delta-shift)), curFrame+delta*fd); //add second +shift            curFrame += (delta*fd + 2*fd); //increment frame            shift++;        }        app.endUndoGroup();    }}   
