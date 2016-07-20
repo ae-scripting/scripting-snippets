62ca149a4496176acf501fe894536ca0c4b3ee58
Alexey Kravchenko <alexus12345@yandex.ru>
2016-07-20 19:35:46 +0600

    Get Layer Type

---
+++ getLayerType.jsx
@@ -0,0 +1,53 @@
+function getLayerType(layer)
+	{
+		try
+			{
+				var src = layer.source;
+				if(src.toString() == "[object Comp]")
+					return "Comp";
+				else 
+					if(!layer.hasVideo && layer.hasAudio)
+						return "Audio";
+					else 
+						if(src.frameDuration == 0)
+							return "Image or Solid";
+						else
+							try
+								{
+									layer("ADBE Effect Parade")("CINEMA 4D Effect");
+									return "Cinema 4D File"
+								}
+							catch(e)
+								{
+									return "Video";
+								}
+			}
+		catch(e)//no source
+			{
+				try
+					{
+						layer("ADBE Text Properties");
+						return "Text";
+					}
+				catch(e)//not text
+					{
+						try
+							{
+								layer("ADBE Light Options Group");
+								return "Light";		
+							}
+						catch(e)//not light
+							{
+								try
+									{
+										layer("ADBE Camera Options Group");
+										return "Camera";
+									}
+								catch(e)
+									{
+										return "Unknown";
+									}
+							}
+					}
+			}
+	}
